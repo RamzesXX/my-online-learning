d3ba609608c25aedb29d5a037531e4b05a4c0cce
Roman Khanchych <roman.khanchych@nixsolutions.com>
2018-01-29 08:40:28 +0200

    Refactors  code

--- coursera/discrete_optimization/knapsack/src/Solver.java
+++ coursera/discrete_optimization/knapsack/src/Solver.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -8,10 +7,43 @@ import java.util.*;
  * The class <code>Solver</code> is an implementation of a greedy algorithm to solve the knapsack problem.
  */
 public class Solver {
-    private int items;
+    static class Item {
+        private int value;
+        private int weight;
+
+        public Item(int value, int weight) {
+            this.value = value;
+            this.weight = weight;
+        }
+    }
+
+    static class BBNode {
+        int potential;
+        int capacity;
+        int value;
+
+        public BBNode(int potential, int capacity, int value) {
+            this.potential = potential;
+            this.capacity = capacity;
+            this.value = value;
+        }
+
+        public int getPotential() {
+            return potential;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private int n;
     private int capacity;
-    private int[] values;
-    private int[] weights;
+    private Item[] items;
 
     public static void main(String[] args) {
         try {
@@ -30,7 +62,7 @@ public class Solver {
         for (String arg : args) {
             if (arg.startsWith("-file=")) {
                 loadInputData(arg.substring(6));
-                int[] solution = fillKnapsackDynamicProgramingAlgorithm();
+                int[] solution = capacity < 100_001 ? fillKnapsackDynamicProgramingAlgorithm() : fillKnapsackBranchAndBounds();
                 print(solution);
 
                 return;
@@ -42,45 +74,51 @@ public class Solver {
         try (BufferedReader input = new BufferedReader(new FileReader(fileName))) {
             Scanner scanner = new Scanner(input);
 
-            items = scanner.nextInt();
+            n = scanner.nextInt();
             capacity = scanner.nextInt();
 
-            values = new int[items];
-            weights = new int[items];
-            for (int i = 0; i < items; i++) {
-                values[i] = scanner.nextInt();
-                weights[i] = scanner.nextInt();
+            items = new Item[n];
+            for (int i = 0; i < n; i++) {
+                items[i] = new Item(scanner.nextInt(), scanner.nextInt());
             }
+            Arrays.sort(items, Comparator.comparingDouble(item -> ( -1.0d * item.value / item.weight)));
         }
     }
-
     // a trivial greedy algorithm for filling the knapsack
-    // it takes items in-order until the knapsack is full
+    // it takes n in-order until the knapsack is full
     private int[] fillKnapsackGreedyAlgorithm() {
         int weight = 0;
-        int[] taken = new int[items];
+        int[] taken = new int[n];
 
-        for (int i = 0; i < items; i++) {
-            taken[i] = (weight + weights[i] <= capacity) ? 1 : 0;
-            weight += weights[i] * taken[i];
+        for (int i = 0; i < n; i++) {
+            taken[i] = (weight + items[i].weight <= capacity) ? 1 : 0;
+            weight += items[i].weight * taken[i];
         }
 
         return taken;
     }
 
+    private int[] fillKnapsackBranchAndBounds() {
+        int[] taken = new int[n];
+
+
+
+        return taken;
+    }
+
     // a trivial greedy algorithm for filling the knapsack
-    // it takes items in-order until the knapsack is full
+    // it takes n in-order until the knapsack is full
     private int[] fillKnapsackDynamicProgramingAlgorithm() {
         Map<Integer, List<Integer>> optimalSolutions = new HashMap<>();
-        int[] taken = new int[items];
+        int[] taken = new int[n];
 
         optimalSolutions.put(0, new ArrayList<>());
-        for (int i = 0; i < items; i++) {
+        for (int i = 0; i < n; i++) {
             Map<Integer, List<Integer>> partialSolutions = new HashMap<>();
 
             for (Map.Entry<Integer, List<Integer>> entry : optimalSolutions.entrySet()) {
-                int newWeight = entry.getKey() + weights[i];
-                int newValue = calcValue(entry.getValue()) + values[i];
+                int newWeight = entry.getKey() + items[i].weight;
+                int newValue = calcValue(entry.getValue()) + items[i].value;
                 int currentValue = calcValue(optimalSolutions.getOrDefault(newWeight, new ArrayList<>()));
 
                 if ((newWeight <= capacity) && (!optimalSolutions.containsKey(newWeight) || (newValue > currentValue))){
@@ -100,19 +138,19 @@ public class Solver {
     }
 
     private int calcValue(List<Integer> items) {
-        return items.stream().mapToInt(item -> values[item]).sum();
+        return items.stream().mapToInt(item -> this.items[item].value).sum();
     }
 
     private void print(int[] taken) {
         int value = 0;
         StringBuilder builder = new StringBuilder();
 
-        for (int i = 0; i < items; i++) {
-            value += taken[i] * values[i];
+        for (int i = 0; i < n; i++) {
+            value += taken[i] * items[i].value;
             builder.append(taken[i]).append(" ");
         }
 
-        System.out.println(value + " 0");
+        System.out.println(value + " 1");
         System.out.println(builder.toString());
         System.out.println("");
     }
