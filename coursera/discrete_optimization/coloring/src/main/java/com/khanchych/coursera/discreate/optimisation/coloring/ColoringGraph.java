2aee136a919f723d36f323e5cc3809deb083e4e7
Roman Khanchych <roman.khanchych@nixsolutions.com>
2018-10-09 01:21:43 +0300

    Coloring - add visualisation

--- coursera/discrete_optimization/coloring/src/main/java/com/khanchych/coursera/discreate/optimisation/coloring/ColoringGraph.java
+++ coursera/discrete_optimization/coloring/src/main/java/com/khanchych/coursera/discreate/optimisation/coloring/ColoringGraph.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ColoringGraph implements Coloring {
+    private Map<Integer, Set<Integer>> items;
+
     @Override
     public List<Integer> solve(ProblemInput input) {
         return solve(input.getItems());
@@ -14,15 +16,13 @@ public class ColoringGraph implements Coloring {
     public List<Integer> solve(final Map<Integer, Set<Integer>> items) {
         Set<Integer> usedColors = new HashSet<>();
         Set<Integer> usedByAdjacentNodesColors = new HashSet<>();
-
+        List<Integer> orderedNodes;
         List<Integer> nodesColors = Stream.iterate(0, n -> 0)
                 .limit(items.size())
                 .collect(Collectors.toList());
-        List<Integer> orderedNodes = Stream.iterate(0, n -> n + 1)
-                .limit(items.size())
-                .sorted(Comparator.comparingInt(element -> items.get(element).size()))
-                .collect(Collectors.toList());
 
+        this.items = items;
+        orderedNodes = orderNodesByBFS();
         usedColors.add(0);
         for (Integer node : orderedNodes) {
             usedByAdjacentNodesColors.clear();
@@ -50,4 +50,35 @@ public class ColoringGraph implements Coloring {
         return nodesColors;
     }
 
+    private List<Integer> orderNodesByDegree() {
+        return Stream.iterate(0, n -> n + 1)
+                .limit(items.size())
+                .sorted(Comparator.comparingInt(element -> items.get(element).size()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> orderNodesByBFS() {
+        List<Integer> orderedItems = new ArrayList<>();
+        Set<Integer> unprocessed = new HashSet<>(items.keySet());
+        while (!unprocessed.isEmpty()) {
+            int element = unprocessed.iterator().next();
+            process(element, unprocessed, orderedItems);
+        }
+
+        return orderedItems;
+    }
+
+    private void process(int startElement, Set<Integer> unprocessed, List<Integer> orderedItems) {
+        Queue<Integer> toProcess = new LinkedList<>();
+
+        toProcess.add(startElement);
+        while (!toProcess.isEmpty()) {
+            int element = toProcess.remove();
+            if (unprocessed.contains(element)) {
+                toProcess.addAll(items.get(element));
+                unprocessed.remove(element);
+                orderedItems.add(element);
+            }
+        }
+    }
 }
