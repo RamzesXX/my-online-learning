043007b411ae136951cdfac1fb3e0a318847fa5b
Roman Khanchych <roman.khanchych@nixsolutions.com>
2019-01-14 00:07:50 +0200

    udemy - inheritence tasks

---
+++ udemy/src/main/java/com/khanchych/udemy/javaindepth/inheritence/p1/A1.java
@@ -0,0 +1,5 @@
+package com.khanchych.udemy.javaindepth.inheritence.p1;
+
+public class A1 {
+    static int defaultAccess;
+}
---
+++ udemy/src/main/java/com/khanchych/udemy/javaindepth/inheritence/p1/B1.java
@@ -0,0 +1,4 @@
+package com.khanchych.udemy.javaindepth.inheritence.p1;
+
+public class B1 extends A1 {
+}
