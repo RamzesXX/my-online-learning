f1d1ee69731e83373a4f2598185a808281858783
Roman Khanchych <roman.khanchych@nixsolutions.com>
2019-02-20 22:20:37 +0200

    leetcode

---
+++ leetcode/src/test/java/com/khanchych/leetcode/Task993Test.java
@@ -0,0 +1,39 @@
+package com.khanchych.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class Task993Test {
+    private Task993 task993;
+
+    @Before
+    public void setUp() throws Exception {
+        task993 = new Task993();
+    }
+
+    @Test
+    public void isCousins() {
+        TreeNode root = createTree(new Integer[]{1, 2, 3, null, 4, null, 5}, 0);
+        assertTrue(task993.isCousins(root, 5, 4));
+    }
+
+    private TreeNode createTree(Integer[] array, int index) {
+        TreeNode node = new TreeNode(array[index]);
+        int childIndex = 2*index + 1;
+
+        if (childIndex < array.length) {
+            if (array[childIndex] != null) {
+                node.left = createTree(array, childIndex);
+            }
+
+            childIndex++;
+            if (array[childIndex] != null) {
+                node.right = createTree(array, childIndex);
+            }
+        }
+        return node;
+    }
+}
+
